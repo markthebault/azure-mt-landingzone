34fb2281b35271db5ffa8b45da615a900357c734
Mark Thebault <mark@pop-os.localdomain>
2020-04-25 17:03:36 +0200

    simple examples

--- infrastructure/landingzone/tflz_network/main.tf
+++ infrastructure/landingzone/tflz_network/main.tf
@@ -1,4 +1,10 @@
+resource "random_string" "random" {
+  length  = 4
+  special = false
+  upper   = false
+}
+
 resource "azurerm_resource_group" "example" {
-  name     = "example"
+  name     = "${random_string.random.result}example"
   location = "West Europe"
 }

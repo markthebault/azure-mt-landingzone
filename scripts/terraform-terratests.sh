39e3389e14dfb1880256ed0496a5415abc532173
Mark Thebault <mark.thebault@gmail.com>
2020-04-26 11:20:58 +0000

    add terratest script

---
+++ scripts/terraform-terratests.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Validates that the modules are declared correctly
+
+
+for file in $(find . -name '*_test.go'); do
+    dir=$(dirname "${file}")
+
+    echo -e "\\n\\n"
+    echo "************ Runnin on directory: $dir"
+    cd "$dir";
+
+    echo -e "provider \"azurerm\" { \\n version = \"=2.6.0\" \\n features {} \\n }" > ../provider.tf
+    echo -e "provider \"azurerm\" { \\n alias = \"shared_services\" \\n version = \"=2.6.0\" \\n features {} \\n }" > ../provider2.tf
+    go test -v 
+
+    #Clean up
+    rm ../provider.tf ../provider2.tf
+    # rm -Rf .terraform
+    cd -;
+done
